e1796f286798f8dced073fd55f42a834a9c2105b
Martynenko K.S <sparkmart@mail.ru>
2020-07-16 15:54:25 +0300

    Start

--- src/main/java/org/mart/springedu/SpringeduApplication.java
+++ src/main/java/org/mart/springedu/SpringeduApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringeduApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringeduApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringeduApplication.class, args);
+    }
 
 }
